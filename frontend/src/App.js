bbf1c0dfb9bb363b385cc39ad10af094f24c5dc6
Bo Thomsen <boh1996@gmail.com>
2018-04-24 18:50:51 +0200

    Split graph into seperate file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,10 @@ import {
   Slider,
   InputNumber
 } from 'antd'
+import Graph from './Graph/Graph'
 import './App.css'
 import axios from 'axios'
 import _ from 'lodash'
-import {Line} from "react-chartjs-2"
 const { Header, Content } = Layout
 const Option = Select.Option
 
@@ -351,70 +351,17 @@ class App extends Component {
                   }
                 </Col>
               </Row>
-              { dataset !== null && data !== null &&
-                <Row>
-                  <Col span={24}>
-                    <Line
-                      height={300}
-                      options={{
-                        maintainAspectRatio: false,
-                        title: {
-                          text: 'Benchmark results'
-                        },
-                        scales: {
-                          xAxes: [{
-                            type: 'time',
-                            time: {
-                              parser: 'MM/DD/YYYY HH:mm',
-                              tooltipFormat: 'll HH:mm'
-                            },
-                            scaleLabel: {
-                              display: true,
-                              labelString: 'Date'
-                            }
-                          }],
-                          yAxes: [{
-                            ticks: {
-                              //min: 1,
-                              max: (graphType === "speedup") ? speedUpMax : undefined
-                            },
-                            scaleLabel: {
-                              display: true,
-                              labelString: 'value'
-                            }
-                          }]
-                        }
-                      }}
-                      data={{
-                        labels: x,
-                        datasets: [
-                          {
-                            label: `${backend}/${machine}/${benchmark}/${dataset}`,
-                            fill: false,
-                            lineTension: 0.1,
-                            backgroundColor: 'rgba(75,192,192,0.4)',
-                            borderColor: 'rgba(75,192,192,1)',
-                            borderCapStyle: 'butt',
-                            borderDash: [],
-                            borderDashOffset: 0.0,
-                            borderJoinStyle: 'miter',
-                            pointBorderColor: 'rgba(75,192,192,1)',
-                            pointBackgroundColor: '#fff',
-                            pointBorderWidth: 1,
-                            pointHoverRadius: 5,
-                            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                            pointHoverBorderColor: 'rgba(220,220,220,1)',
-                            pointHoverBorderWidth: 2,
-                            pointRadius: 1,
-                            pointHitRadius: 10,
-                            data: y
-                          }
-                        ]
-                      }}
-                    />
-                  </Col>
-                </Row>
-              }
+              <Graph
+                dataset={dataset}
+                data={data}
+                backend={backend}
+                speedUpMax={speedUpMax}
+                graphType={graphType}
+                benchmark={benchmark}
+                machine={machine}
+                x={x}
+                y={y}
+              />
             </Content>
           </Layout>
         </Layout>
--- frontend/src/Graph/Graph.js
+++ frontend/src/Graph/Graph.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import {
+  Row,
+  Col
+} from 'antd'
+import {Line} from "react-chartjs-2"
 
 class Graph extends Component {
   static propTypes = {
@@ -13,10 +17,84 @@ class Graph extends Component {
   }
 
   render() {
+    const {
+      dataset,
+      data,
+      backend,
+      speedUpMax,
+      graphType,
+      benchmark,
+      machine,
+      x,
+      y
+    } = this.props
+
+    if ( dataset === null || data === null )
+      return null
+
     return (
-      <div>
-        
-      </div>
+      <Row>
+        <Col span={24}>
+          <Line
+            height={300}
+            options={{
+              maintainAspectRatio: false,
+              title: {
+                text: 'Benchmark results'
+              },
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  time: {
+                    parser: 'MM/DD/YYYY HH:mm',
+                    tooltipFormat: 'll HH:mm'
+                  },
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Date'
+                  }
+                }],
+                yAxes: [{
+                  ticks: {
+                    //min: 1,
+                    max: (graphType === "speedup") ? speedUpMax : undefined
+                  },
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'value'
+                  }
+                }]
+              }
+            }}
+            data={{
+              labels: x,
+              datasets: [
+                {
+                  label: `${backend}/${machine}/${benchmark}/${dataset}`,
+                  fill: false,
+                  lineTension: 0.1,
+                  backgroundColor: 'rgba(75,192,192,0.4)',
+                  borderColor: 'rgba(75,192,192,1)',
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: 'rgba(75,192,192,1)',
+                  pointBackgroundColor: '#fff',
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                  pointHoverBorderColor: 'rgba(220,220,220,1)',
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: y
+                }
+              ]
+            }}
+          />
+        </Col>
+      </Row>
     )
   }
 }
