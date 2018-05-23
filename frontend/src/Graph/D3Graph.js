97663eb9252847d18d90d2364302bc36ec019efe
fzzle <e@weihe.dk>
2018-05-23 20:26:14 +0200

    Refactor2

--- frontend/src/Graph/D3Graph.js
+++ frontend/src/Graph/D3Graph.js
@@ -13,7 +13,143 @@ class D3Graph extends Component {
 
   // Initialize chart container
   componentDidMount() {
-    this._initialize()
+    // Initialize selected view
+    let at = findDOMNode(this)
+    this.container = d3.select(at)
+    this.selected = this.container.append('svg')
+    this.selected.classed('selected', true)
+
+    this.selected.on('mousemove', () => {
+      if (this.datasets.length === 0) return
+
+      let bisect = d3.bisector(d => d.x).left;
+      let x = d3.mouse(this.selected.node())[0]
+      let actual = this.selectedXScale.invert(x)
+
+      // Find potential closest
+      // points to the cursor
+      let potentials = []
+      for (let i in this.datasets) {
+        let {data} = this.datasets[i]
+        let j = bisect(data, actual, 1)
+        p.push({data: data[j], i})
+        p.push({data: data[j - 1], i})
+      }
+
+      // Remove undefined
+      potentials = potentials.filter(x => x.d)
+
+      // Sort by closest
+      potentials = p.sort((a, b) => {
+        let x0 = Math.abs(a.data.x - actual)
+        let x1 = Math.abs(b.data.x - actual)
+        return d3.descending(x1, x0)
+      })
+
+      // Return if no points
+      if (p.length === 0) return
+
+      // Only keep the commits with
+      // the same hash as the closest
+      potentials = p.filter(x => potentials[0].d.commit === x.d.commit)
+
+      // Find offset
+      x = this.selectedXScale(potentials[0].d.x)
+
+      d3.select('.caret')
+        .attr('x1', x)
+        .attr('x2', x)
+    })
+
+    this.selected.append('line')
+      .classed('caret', true)
+      .attr('y2', '100%')
+
+    // Initialize x-overview
+    this.overview = this.container.append('svg')
+    this.overview.classed('x-overview', true)
+
+    // Additional rescale drag handlers
+    slider.on('drag', this._rescale)
+    handle.on('drag', this._rescale)
+
+    var sliderGroup = this.overview.append('g')
+
+    // Append slider
+    this.slider = sliderGroup.append('rect')
+      .classed('slider', true)
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('x', '0%')
+      .call(slider)
+
+    // Append west handle
+    sliderGroup.append('rect')
+      .classed('handle', true)
+      .attr('height', '100%')
+      .attr('x', '0%')
+      .call(handle)
+
+    // Append east handle
+    sliderGroup.append('rect')
+      .classed('handle', true)
+      .attr('height', '100%')
+      .attr('x', '100%')
+      .call(handle)
+
+    // Initialize scales
+    this.selectedXScale = d3.scaleTime()
+    this.selectedYScale = d3.scaleLinear()
+    this.overviewXScale = d3.scaleTime()
+    this.overviewYScale = d3.scaleLinear()
+
+    // Keep valueline within range
+    this.selectedXScale.clamp(true)
+
+    // Selected valueline
+    this.selectedValueline = d3.line()
+      .x(d => this.selectedXScale(d.x))
+      .y(d => this.selectedYScale(d.y))
+
+    // x-overview valueline
+    this.overviewValueline = d3.line()
+      .x(d => this.overviewXScale(d.x))
+      .y(d => this.overviewYScale(d.y))
+
+    // Standard deviation area
+    this.stdDevArea = d3.area()
+      .x(d => this.selectedXScale(d.x))
+      .y0(d => this.selectedYScale(d.y - d.stdDev))
+      .y1(d => this.selectedYScale(d.y + d.stdDev))
+
+    // Initialize axes
+    this.xAxis = this.selected.append('g')
+    this.yAxis = this.selected.append('g')
+
+    this.xAxis.classed('y-axis', true)
+    this.yAxis.classed('x-axis', true)
+
+    this.xComponent = d3.axisBottom(this.selectedXScale)
+    this.yComponent = d3.axisLeft(this.selectedYScale)
+
+    this.xComponent.tickSizeOuter(0)
+    this.yComponent.tickSizeOuter(0)
+    this.xComponent.tickPadding(8)
+    this.yComponent.tickPadding(8)
+
+    // Initialize graph containers
+    this.selectedGraphs = this.selected.append('g')
+    this.overviewGraphs = this.overview.append('g')
+
+    // Clamp y-axis to height
+    this.selected.append('defs')
+      .append('clipPath')
+      .attr('id', 'd3-clip')
+      .append('rect')
+    this.selectedGraphs.attr('clip-path', 'url(#d3-clip)')
+
+    // Size the chart
+    this._resize()
   }
 
   // Render via d3
@@ -60,24 +196,24 @@ class D3Graph extends Component {
     this.selectedYScale.domain(yDomain)
 
     // Redraw axes
-    this.x_axis.call(this.x_component)
-    this.y_axis.call(this.y_component)
+    this.xAxis.call(this.xComponent)
+    this.yAxis.call(this.yComponent)
 
     for (let dataset of this.datasets) {
       let {data, color} = dataset
 
       // Create paths for the dataset
-      let stdDevPath = this.s_graphs.append('path')
-      let selectedPath = this.s_graphs.append('path')
-      let overviewPath = this.o_graphs.append('path')
+      let stdDevPath = this.selectedGraphs.append('path')
+      let selectedPath = this.selectedGraphs.append('path')
+      let overviewPath = this.overviewGraphs.append('path')
 
       stdDevPath.classed('d3-area', true)
       selectedPath.classed('d3-graph', true)
       overviewPath.classed('d3-graph', true)
 
       stdDevPath.attr('d', this.stdDevArea(data))
-      selectedPath.attr('d', this.s_valueline(data))
-      overviewPath.attr('d', this.o_valueline(data))
+      selectedPath.attr('d', this.selectedValueline(data))
+      overviewPath.attr('d', this.overviewValueline(data))
 
       // Store references to paths
       dataset.stdDevPath = stdDevPath
@@ -105,148 +241,6 @@ class D3Graph extends Component {
     )
   }
 
-
-  _initialize() {
-    // Initialize selected view
-    let at = findDOMNode(this)
-    this.container = d3.select(at)
-    this.selected = this.container.append('svg')
-    this.selected.classed('selected', true)
-
-    this.selected.on('mousemove', () => {
-      if (this.datasets.length === 0) return
-
-      let bisect = d3.bisector(d => d.x).left;
-      let x = d3.mouse(this.selected.node())[0]
-      let actual = this.selectedXScale.invert(x)
-
-      // Find potential closest
-      // points to the cursor
-      let p = []
-      for (let i in this.datasets) {
-        let {data} = this.datasets[i]
-        let j = bisect(data, actual, 1)
-        p.push({d: data[j], i})
-        p.push({d: data[j - 1], i})
-      }
-
-      // Remove undefined
-      p = p.filter(x => x.d)
-
-      // Sort by closest
-      p = p.sort((a, b) => {
-        let x0 = Math.abs(a.d.x - actual)
-        let x1 = Math.abs(b.d.x - actual)
-        return d3.descending(x1, x0)
-      })
-
-      // Return if no points
-      if (p.length === 0) return
-
-      // Only keep the commits with
-      // the same hash as the closest
-      p = p.filter(x => p[0].d.commit === x.d.commit)
-
-      // Find offset
-      x = this.selectedXScale(p[0].d.x)
-
-      d3.select('.caret')
-        .attr('x1', x)
-        .attr('x2', x)
-    })
-
-    this.selected.append('line')
-      .classed('caret', true)
-      .attr('y2', '100%')
-
-    // Initialize x-overview
-    this.overview = this.container.append('svg')
-    this.overview.classed('x-overview', true)
-
-    // Additional rescale drag handlers
-    slider.on('drag', this._rescale)
-    handle.on('drag', this._rescale)
-
-    // Slider group
-    var g = this.overview.append('g')
-
-    // Append slider
-    this.slider = g.append('rect')
-      .classed('slider', true)
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('x', '0%')
-      .call(slider)
-
-    // Append west handle
-    g.append('rect')
-      .classed('handle', true)
-      .attr('height', '100%')
-      .attr('x', '0%')
-      .call(handle)
-
-    // Append east handle
-    g.append('rect')
-      .classed('handle', true)
-      .attr('height', '100%')
-      .attr('x', '100%')
-      .call(handle)
-
-    // Initialize scales
-    this.selectedXScale = d3.scaleTime()
-    this.selectedYScale = d3.scaleLinear()
-    this.overviewXScale = d3.scaleTime()
-    this.overviewYScale = d3.scaleLinear()
-
-    // Keep valueline within range
-    this.selectedXScale.clamp(true)
-
-    // Selected valueline
-    this.s_valueline = d3.line()
-      .x(d => this.selectedXScale(d.x))
-      .y(d => this.selectedYScale(d.y))
-
-    // x-overview valueline
-    this.o_valueline = d3.line()
-      .x(d => this.overviewXScale(d.x))
-      .y(d => this.overviewYScale(d.y))
-
-    // Standard deviation area
-    this.stdDevArea = d3.area()
-      .x(d => this.selectedXScale(d.x))
-      .y0(d => this.selectedYScale(d.y - d.stdDev))
-      .y1(d => this.selectedYScale(d.y + d.stdDev))
-
-    // Initialize axes
-    this.x_axis = this.selected.append('g')
-    this.y_axis = this.selected.append('g')
-
-    this.x_axis.classed('y-axis', true)
-    this.y_axis.classed('x-axis', true)
-
-    this.x_component = d3.axisBottom(this.selectedXScale)
-    this.y_component = d3.axisLeft(this.selectedYScale)
-
-    this.x_component.tickSizeOuter(0)
-    this.y_component.tickSizeOuter(0)
-    this.x_component.tickPadding(8)
-    this.y_component.tickPadding(8)
-
-    // Initialize graph containers
-    this.s_graphs = this.selected.append('g')
-    this.o_graphs = this.overview.append('g')
-
-    // Clamp y-axis to height
-    this.selected.append('defs')
-      .append('clipPath')
-      .attr('id', 'd3-clip')
-      .append('rect')
-    this.s_graphs.attr('clip-path', 'url(#d3-clip)')
-
-    // Size the chart
-    this._resize()
-  }
-
   // Rescale the x-axis based on the
   // position and size of the slider
   _rescale = () => {
@@ -268,12 +262,12 @@ class D3Graph extends Component {
     for (let dataset of this.datasets) {
       let {data, stdDevPath, selectedPath} = dataset
       stdDevPath.attr('d', this.stdDevArea(data))
-      selectedPath.attr('d', this.s_valueline(data))
+      selectedPath.attr('d', this.selectedValueline(data))
     }
 
     // Redraw axes
-    this.x_axis.call(this.x_component)
-    this.y_axis.call(this.y_component)
+    this.xAxis.call(this.xComponent)
+    this.yAxis.call(this.yComponent)
   }
 
   // Resize the chart to the
@@ -296,26 +290,26 @@ class D3Graph extends Component {
     let xTicks = Math.max(2, selectedRect.width / 100)
     let yTicks = Math.max(2, selectedRect.height / 30)
 
-    this.x_component.ticks(xTicks)
-    this.y_component.ticks(yTicks)
+    this.xComponent.ticks(xTicks)
+    this.yComponent.ticks(yTicks)
 
     // Update tick line width / height
-    this.x_component.tickSizeInner(-selectedRect.height)
-    this.y_component.tickSizeInner(-selectedRect.width)
+    this.xComponent.tickSizeInner(-selectedRect.height)
+    this.yComponent.tickSizeInner(-selectedRect.width)
 
     // Move x-axis to the bottom
-    this.x_axis.attr('transform', `translate(0,${selectedRect.height})`)
+    this.xAxis.attr('transform', `translate(0,${selectedRect.height})`)
 
     // Redraw axes
-    this.x_axis.call(this.x_component)
-    this.y_axis.call(this.y_component)
+    this.xAxis.call(this.xComponent)
+    this.yAxis.call(this.yComponent)
 
     // Resize graph paths
     for (let dataset of this.datasets) {
       let {data, stdDevPath, selectedPath, overviewPath} = dataset
       stdDevPath.attr('d', this.stdDevArea(data))
-      selectedPath.attr('d', this.s_valueline(data))
-      overviewPath.attr('d', this.o_valueline(data))
+      selectedPath.attr('d', this.selectedValueline(data))
+      overviewPath.attr('d', this.overviewValueline(data))
     }
   }
 
